5db8d62e7f24dacb8b07af237b6c205cf777170f
flowz0 <billylflowers@gmail.com>
2025-07-01 15:23:23 -0700

    update allowedOrigins

--- src/server.js
+++ src/server.js
@@ -14,9 +14,22 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // middleware
+const allowedOrigins = [
+  "https://dd-frontend-liart.vercel.app/",
+  "https://dd-frontend-flowz0s-projects.vercel.app/",
+  "https://dd-frontend-git-main-flowz0s-projects.vercel.app/",
+  "https://dd-frontend-95q5wgv0y-flowz0s-projects.vercel.app/"
+];
+
 app.use(cors({
-  origin: "https://dd-frontend-flowz0s-projects.vercel.app",
-  credentials: true,
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
 app.use(express.json()); // parse JSON bodies: req.body
 app.use(cookieParser());

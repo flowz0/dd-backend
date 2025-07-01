a8a43c5f4918ca694f61226f92fd971d782bc094
flowz0 <billylflowers@gmail.com>
2025-07-01 15:25:56 -0700

    update cors origin url

--- src/server.js
+++ src/server.js
@@ -14,22 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // middleware
-const allowedOrigins = [
-  "https://dd-frontend-liart.vercel.app/",
-  "https://dd-frontend-flowz0s-projects.vercel.app/",
-  "https://dd-frontend-git-main-flowz0s-projects.vercel.app/",
-  "https://dd-frontend-95q5wgv0y-flowz0s-projects.vercel.app/"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: "https://dd-frontend-liart.vercel.app",
+  credentials: true,
 }));
 app.use(express.json()); // parse JSON bodies: req.body
 app.use(cookieParser());
